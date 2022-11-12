4e646b8de8bcc6a1cc57ad70ecadeeb6f406a05b
SajjaniUmayangana <sumayangana2428@gmail.com>
2022-11-12 11:43:00 +0530

    Database Connection

---
+++ database/connection.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+// const mongo_url = "mongodb+srv://sajjani:sajjani123@cluster0.jd5hev6.mongodb.net/?retryWrites=true&w=majority"
+
+const connectDB = async() =>{
+    try{
+        // mongodb connection string
+        const con = await mongoose.connect(mongo_url);
+        console.log('MongoDB database connected');
+
+    }catch(err){
+        console.log(err);
+        process.exit(1); // exit from the process (1 = true )
+        }
+}
+
+module.exports = connectDB;
